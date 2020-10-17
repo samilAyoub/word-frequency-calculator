d525252c5732267ea4de7e923250de7b8b06e05c
Samil Ayoub <ssamil.ayoub@gmail.com>
2020-10-17 10:34:19 +0100

    Feat: Count words in HTML text

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import operator
 import os
 import re
 from collections import Counter
-import exceptions
+
 import nltk
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 
+import exceptions
 from models import db
+from stop_words import stops
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -34,11 +39,38 @@ def request_handler(url):
     return r
 
 
+def html_text_preprocessing(html_text):
+    """
+    Count words in a HTML text.
+
+    :param html_text: HTML text
+    :type html_text: String
+    :returns: Count each word in HTML text
+    :rtype: Counter
+    """
+
+    # Clean text by removing HTML tags.
+    raw = BeautifulSoup(html_text, 'html.parser').get_text()
+    logger.debug('Text after removing HTML tags from: %s', raw[:100])
+    # Set ntlk path.
+    nltk.data.path.append('./nltk_data/')
+    tokens = nltk.word_tokenize(raw)
+    logger.debug('Text tokens: %s', tokens[:100])
+    nltk_text = nltk.Text(tokens)
+    # Remove punctuation.
+    no_punct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in nltk_text if no_punct.match(w)]
+    # Eliminate stop words.
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+    logger.debug('10 most common elements: %s',
+                 no_stop_words_count.most_common(10))
+    return no_stop_words_count
+
+
 @app.route("/", methods=['POST', 'GET'])
 def hello():
     errors = []
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
     if request.method == 'POST':
         # Get URL that user has entred
         url = request.form['url']
@@ -49,6 +81,8 @@ def hello():
         except exceptions.GetRequestException as e:
             msg = str(e)
             errors.append(msg)
+        if r:
+            html_text_preprocessing(r.text)
     return render_template('index.html')
 
 
